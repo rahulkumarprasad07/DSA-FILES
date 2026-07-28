6c667e666a99fd125ffd103252d2a68d38b97289
rahulkumarprasad07 <rahul.kp1024@gmail.com>
2026-07-28 20:24:46 +0530

    just checking

--- array_11july.cpp
+++ array_11july.cpp
@@ -1,3 +1,57 @@
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int rows, cols;
+//     cin >> rows >> cols;
+
+//     int arr[100][100];
+
+//     // Input
+//     for (int i = 0; i < rows; i++) {
+//         for (int j = 0; j < cols; j++) {
+//             cin >> arr[i][j];
+//         }
+//     }
+
+//     int startRow = 0;
+//     int endRow = rows - 1;
+//     int startCol = 0;
+//     int endCol = cols - 1;
+
+//     while (startRow <= endRow && startCol <= endCol) {
+
+//         // Top Row
+//         for (int i = startCol; i <= endCol; i++) {
+//             cout << arr[startRow][i] << " ";
+//         }
+//         startRow++;
+
+//         // Right Column
+//         for (int i = startRow; i <= endRow; i++) {
+//             cout << arr[i][endCol] << " ";
+//         }
+//         endCol--;
+
+//         // Bottom Row
+//         if (startRow <= endRow) {
+//             for (int i = endCol; i >= startCol; i--) {
+//                 cout << arr[endRow][i] << " ";
+//             }
+//             endRow--;
+//         }
+
+//         // Left Column
+//         if (startCol <= endCol) {
+//             for (int i = endRow; i >= startRow; i--) {
+//                 cout << arr[i][startCol] << " ";
+//             }
+//             startCol++;
+//         }
+//     }
+
+//     return 0;
+// }
 #include <iostream>
 using namespace std;
 
@@ -14,40 +68,15 @@ int main() {
         }
     }
 
-    int startRow = 0;
-    int endRow = rows - 1;
-    int startCol = 0;
-    int endCol = cols - 1;
+    // Row-wise Sum
+    for (int i = 0; i < rows; i++) {
+        int sum = 0;
 
-    while (startRow <= endRow && startCol <= endCol) {
-
-        // Top Row
-        for (int i = startCol; i <= endCol; i++) {
-            cout << arr[startRow][i] << " ";
-        }
-        startRow++;
-
-        // Right Column
-        for (int i = startRow; i <= endRow; i++) {
-            cout << arr[i][endCol] << " ";
-        }
-        endCol--;
-
-        // Bottom Row
-        if (startRow <= endRow) {
-            for (int i = endCol; i >= startCol; i--) {
-                cout << arr[endRow][i] << " ";
-            }
-            endRow--;
+        for (int j = 0; j < cols; j++) {
+            sum += arr[i][j];
         }
 
-        // Left Column
-        if (startCol <= endCol) {
-            for (int i = endRow; i >= startRow; i--) {
-                cout << arr[i][startCol] << " ";
-            }
-            startCol++;
-        }
+        cout << "Sum of Row " << i + 1 << " = " << sum << endl;
     }
 
     return 0;
