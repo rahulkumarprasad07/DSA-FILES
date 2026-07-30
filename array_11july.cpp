c61c7b0650d2b8fbb2558a1c34866c03fe80d5cc
rahulkumarprasad07 <rahul.kp1024@gmail.com>
2026-07-30 22:26:17 +0530

    added

--- array_11july.cpp
+++ array_11july.cpp
@@ -113,42 +113,66 @@
 // }
 // #include <iostream>
 // #
-#include <iostream>
-#include <cstdlib>
-#include <ctime>
+/#include <iostream>
+// #include <cstdlib>
+// #include <ctime>
 
+// using namespace std;
+
+// int main() {
+//     srand(time(0));
+
+//     int secretNumber = rand() % 100 + 1;
+//     int guess;
+//     int attempts = 0;
+
+//     cout << "=============================\n";
+//     cout << "   NUMBER GUESSING GAME\n";
+//     cout << "=============================\n";
+//     cout << "Guess a number between 1 and 100.\n\n";
+
+//     do {
+//         cout << "Enter your guess: ";
+//         cin >> guess;
+//         attempts++;
+
+//         if (guess > secretNumber) {
+//             cout << "Too High!\n\n";
+//         }
+//         else if (guess < secretNumber) {
+//             cout << "Too Low!\n\n";
+//         }
+//         else {
+//             cout << "\nCongratulations! 🎉\n";
+//             cout << "You guessed the correct number.\n";
+//             cout << "Attempts: " << attempts << endl;
+//         }
+
+//     } while (guess != secretNumber);
+
+//     return 0;
+// }
+#include <iostream>
+#include <vector>
 using namespace std;
 
+vector<int> twoSum(vector<int>& nums, int target) {
+    for (int i = 0; i < nums.size(); i++) {
+        for (int j = i + 1; j < nums.size(); j++) {
+            if (nums[i] + nums[j] == target) {
+                return {i, j};
+            }
+        }
+    }
+    return {};
+}
+
 int main() {
-    srand(time(0));
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
 
-    int secretNumber = rand() % 100 + 1;
-    int guess;
-    int attempts = 0;
+    vector<int> ans = twoSum(nums, target);
 
-    cout << "=============================\n";
-    cout << "   NUMBER GUESSING GAME\n";
-    cout << "=============================\n";
-    cout << "Guess a number between 1 and 100.\n\n";
-
-    do {
-        cout << "Enter your guess: ";
-        cin >> guess;
-        attempts++;
-
-        if (guess > secretNumber) {
-            cout << "Too High!\n\n";
-        }
-        else if (guess < secretNumber) {
-            cout << "Too Low!\n\n";
-        }
-        else {
-            cout << "\nCongratulations! 🎉\n";
-            cout << "You guessed the correct number.\n";
-            cout << "Attempts: " << attempts << endl;
-        }
-
-    } while (guess != secretNumber);
-
+    cout << "Indices: " << ans[0] << " " << ans[1];
     return 0;
 }
