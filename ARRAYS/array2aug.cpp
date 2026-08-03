04c86a5117ea252761d8130113d573d9ec8132df
rahulkumarprasad07 <rahul.kp1024@gmail.com>
2026-08-03 22:43:49 +0530

    adding 3aug

--- ARRAYS/array2aug.cpp
+++ ARRAYS/array2aug.cpp
@@ -14,31 +14,69 @@
 
 //     return 0;
 // }
+// #include <iostream>
+// #include <climits>
+// using namespace std;
+
+// int main() {
+//     int arr[] = {12, 35, 1, 10, 34, 1, 35};
+//     int n = sizeof(arr) / sizeof(arr[0]);
+
+//     int largest = INT_MIN;
+//     int secondLargest = INT_MIN;
+
+//     for (int i = 0; i < n; i++) {
+
+//         if (arr[i] > largest) {
+//             secondLargest = largest;
+//             largest = arr[i];
+//         }
+
+//         else if (arr[i] > secondLargest && arr[i] != largest) {
+//             secondLargest = arr[i];
+//         }
+//     }
+
+//     if (secondLargest == INT_MIN)
+//         cout << "Second largest element does not exist";
+//     else
+//         cout << "Second Largest = " << secondLargest;
+
+//     return 0;
+// }
 #include <iostream>
 #include <climits>
 using namespace std;
 
 int main() {
-    int arr[] = {12, 35, 1, 10, 34, 1, 35};
-    int n = sizeof(arr) / sizeof(arr[0]);
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
 
     int largest = INT_MIN;
     int secondLargest = INT_MIN;
 
     for (int i = 0; i < n; i++) {
-
         if (arr[i] > largest) {
             secondLargest = largest;
             largest = arr[i];
         }
-
         else if (arr[i] > secondLargest && arr[i] != largest) {
             secondLargest = arr[i];
         }
     }
 
+    cout << "Largest = " << largest << endl;
+
     if (secondLargest == INT_MIN)
-        cout << "Second largest element does not exist";
+        cout << "Second Largest does not exist";
     else
         cout << "Second Largest = " << secondLargest;
 
