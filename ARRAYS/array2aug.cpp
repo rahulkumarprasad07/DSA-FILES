b4ba7340a3b4f46940f0a3003ba671794bc89d3d
rahulkumarprasad07 <rahul.kp1024@gmail.com>
2026-08-04 22:34:34 +0530

    adding 4aug

--- ARRAYS/array2aug.cpp
+++ ARRAYS/array2aug.cpp
@@ -1,37 +1,290 @@
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
-int main() {
-    int n;
-    cin >> n;
+// int main() {
+//     int n;
+//     cin >> n;
 
-    int arr[n];
+//     int arr[n];
 
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
-    }
+//     for (int i = 0; i < n; i++) {
+//         cin >> arr[i];
+//     }
 
-    bool found = false;
+//     bool found = false;
 
-    for (int i = 0; i < n; i++) {
-        int count = 0;
+//     for (int i = 0; i < n; i++) {
+//         int count = 0;
 
-        for (int j = i + 1; j < n; j++) {
-            if (arr[i] == arr[j]) {
-                count++;
-            }
-        }
+//         for (int j = i + 1; j < n; j++) {
+//             if (arr[i] == arr[j]) {
+//                 count++;
+//             }
+//         }
 
-        if (count > 0) {
-            cout << "First Repeating Element = " << arr[i];
-            found = true;
-            break;
-        }
-    }
+//         if (count > 0) {
+//             cout << "First Repeating Element = " << arr[i];
+//             found = true;
+//             break;
+//         }
+//     }
 
-    if (!found) {
-        cout << "No repeating element";
-    }
+//     if (!found) {
+//         cout << "No repeating element";
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int n;
+//     cin >> n;
+
+//     int arr[n];
+
+//     for (int i = 0; i < n; i++) {
+//         cin >> arr[i];
+//     }
+
+//     bool found = false;
+
+//     for (int i = 0; i < n; i++) {
+//         int count = 0;
+
+//         for (int j = i + 1; j < n; j++) {
+//             if (arr[i] == arr[j]) {
+//                 count++;
+//             }
+//         }
+
+//         if (count > 0) {
+//             cout << "First Repeating Element = " << arr[i];
+//             found = true;
+//             break;
+//         }
+//     }
+
+//     if (!found) {
+//         cout << "No repeating element";
+//     }
+
+//     return 0;
+// }// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int n;
+//     cin >> n;
+
+//     int arr[n];
+
+//     for (int i = 0; i < n; i++) {
+//         cin >> arr[i];
+//     }
+
+//     bool found = false;
+
+//     for (int i = 0; i < n; i++) {
+//         int count = 0;
+
+//         for (int j = i + 1; j < n; j++) {
+//             if (arr[i] == arr[j]) {
+//                 count++;
+//             }
+//         }
+
+//         if (count > 0) {
+//             cout << "First Repeating Element = " << arr[i];
+//             found = true;
+//             break;
+//         }
+//     }
+
+//     if (!found) {
+//         cout << "No repeating element";
+//     }
+
+//     return 0;
+// }// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int n;
+//     cin >> n;
+
+//     int arr[n];
+
+//     for (int i = 0; i < n; i++) {
+//         cin >> arr[i];
+//     }
+
+//     bool found = false;
+
+//     for (int i = 0; i < n; i++) {
+//         int count = 0;
+
+//         for (int j = i + 1; j < n; j++) {
+//             if (arr[i] == arr[j]) {
+//                 count++;
+//             }
+//         }
+
+//         if (count > 0) {
+//             cout << "First Repeating Element = " << arr[i];
+//             found = true;
+//             break;
+//         }
+//     }
+
+//     if (!found) {
+//         cout << "No repeating element";
+//     }
+
+//     return 0;
+// }// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int n;
+//     cin >> n;
+
+//     int arr[n];
+
+//     for (int i = 0; i < n; i++) {
+//         cin >> arr[i];
+//     }
+
+//     bool found = false;
+
+//     for (int i = 0; i < n; i++) {
+//         int count = 0;
+
+//         for (int j = i + 1; j < n; j++) {
+//             if (arr[i] == arr[j]) {
+//                 count++;
+//             }
+//         }
+
+//         if (count > 0) {
+//             cout << "First Repeating Element = " << arr[i];
+//             found = true;
+//             break;
+//         }
+//     }
+
+//     if (!found) {
+//         cout << "No repeating element";
+//     }
+
+//     return 0;
+// }// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int n;
+//     cin >> n;
+
+//     int arr[n];
+
+//     for (int i = 0; i < n; i++) {
+//         cin >> arr[i];
+//     }
+
+//     bool found = false;
+
+//     for (int i = 0; i < n; i++) {
+//         int count = 0;
+
+//         for (int j = i + 1; j < n; j++) {
+//             if (arr[i] == arr[j]) {
+//                 count++;
+//             }
+//         }
+
+//         if (count > 0) {
+//             cout << "First Repeating Element = " << arr[i];
+//             found = true;
+//             break;
+//         }
+//     }
+
+//     if (!found) {
+//         cout << "No repeating element";
+//     }
+
+//     return 0;
+// }// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int n;
+//     cin >> n;
+
+//     int arr[n];
+
+//     for (int i = 0; i < n; i++) {
+//         cin >> arr[i];
+//     }
+
+//     bool found = false;
+
+//     for (int i = 0; i < n; i++) {
+//         int count = 0;
+
+//         for (int j = i + 1; j < n; j++) {
+//             if (arr[i] == arr[j]) {
+//                 count++;
+//             }
+//         }
+
+//         if (count > 0) {
+//             cout << "First Repeating Element = " << arr[i];
+//             found = true;
+//             break;
+//         }
+//     }
+
+//     if (!found) {
+//         cout << "No repeating element";
+//     }
+
+//     return 0;
+// }// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int n;
+//     cin >> n;
+
+//     int arr[n];
+
+//     for (int i = 0; i < n; i++) {
+//         cin >> arr[i];
+//     }
+
+//     bool found = false;
+
+//     for (int i = 0; i < n; i++) {
+//         int count = 0;
+
+//         for (int j = i + 1; j < n; j++) {
+//             if (arr[i] == arr[j]) {
+//                 count++;
+//             }
+//         }
+
+//         if (count > 0) {
+//             cout << "First Repeating Element = " << arr[i];
+//             found = true;
+//             break;
+//         }
+//     }
+
+//     if (!found) {
+//         cout << "No repeating element";
+//     }
+
+//     return 0;
+// }
