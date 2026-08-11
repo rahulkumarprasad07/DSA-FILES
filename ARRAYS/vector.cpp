8f838d725c31394096ffcedccf239831ee41814b
rahulkumarprasad07 <rahul.kp1024@gmail.com>
2026-08-11 20:29:30 +0530

    adding

--- ARRAYS/vector.cpp
+++ ARRAYS/vector.cpp
@@ -1,27 +1,44 @@
 #include <iostream>
 #include <vector>
 using namespace std;
- void inputMatrix(vector<vector<int>>&arr){
-   int row=arr.size();
-  cout<<" input values:"<<endl;
+//  void inputMatrix(vector<vector<int>>&arr){
+//    int row=arr.size();
+//   cout<<" input values:"<<endl;
   
-   for(int i=0;i<row;i++){
-    for(int j=0;j<arr[i].size();j++){
-        cin>>arr[i][j];
+//    for(int i=0;i<row;i++){
+//     for(int j=0;j<arr[i].size();j++){
+//         cin>>arr[i][j];
+//     }
+//    }
+//    cout<<"required matrix:"<<endl;
+//    for(int i=0;i<row;i++){
+//     for(int j=0;j<arr[i].size();j++){
+//         cout<<arr[i][j]<<" ";
+//     }
+//     cout<<endl;
+//    }
+//  }
+void netSum(vector<vector<int>>&arr){
+    int row=arr.size();
+
+    cout<<"input values:";
+    for(int i=0;i<row;i++){
+                int sum=0;
+        for(int j=0;j<arr[i].size();j++){
+                cin>>arr[i][j];
+                sum+=arr[i][j];
+               
+        }
+         cout<<"the sum of row "<<i+1<<" is "<<sum<<endl;
+         cout<<"enter"<< i+1<<" row:"<<endl;
     }
-   }
-   cout<<"required matrix:"<<endl;
-   for(int i=0;i<row;i++){
-    for(int j=0;j<arr[i].size();j++){
-        cout<<arr[i][j]<<" ";
-    }
-    cout<<endl;
-   }
- }
+    
+
+}
 
 int main(){
     vector<vector<int>>test(3,vector<int>(3));
-    inputMatrix(test);
+    netSum(test);
 
 
 
