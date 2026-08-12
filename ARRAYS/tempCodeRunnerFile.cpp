395275986f2ac2f57df4713f4dc125fcbdb31df4
rahulkumarprasad07 <rahul.kp1024@gmail.com>
2026-08-12 17:24:15 +0530

    adding 12aug:

--- ARRAYS/tempCodeRunnerFile.cpp
+++ ARRAYS/tempCodeRunnerFile.cpp
@@ -1,6 +1,12 @@
-#include <iostream>
-using namespace std;
-int main (){
-    cout<<"hello RAHUL"<<endl;
-    return 0;
+(int i=0;i<cols;i++){
+        for(int j=0;j<rows;j++){
+            cout<<ulta[j][i]<<" ";
+        }
+        cout<<endl;
+    }
+}
+int main(){
+    vector<vector<int>>test(3,vector<int>(4));
+    transpose(test);
+
 }
--- ARRAYS/vector.cpp
+++ ARRAYS/vector.cpp
@@ -166,44 +166,87 @@ using namespace std;
 
 
 //Matrix ka transpose karo(square matrix).
-void trasnsposeMatrix(vector<vector<int>>&arr){
-    int row=arr.size();
+// void trasnsposeMatrix(vector<vector<int>>&arr){
+//     int row=arr.size();
    
 
 
-    cout<<"input values:"<<endl;
-    for(int i=0;i<row;i++){
+//     cout<<"input values:"<<endl;
+//     for(int i=0;i<row;i++){
                 
-        for(int j=0;j<arr[i].size();j++){
-                cin>>arr[i][j];
+//         for(int j=0;j<arr[i].size();j++){
+//                 cin>>arr[i][j];
             
-        }
+//         }
          
-    }
-vector<vector<int>>transpose=arr;
-int transposeRows=transpose.size();
-for(int i=0;i<transposeRows;i++){
-    for(int j=0;j<transpose[i].size();j++){
-        if(i<=j){
-            transpose[i][j]=transpose[j][i];
+//     }
+// vector<vector<int>>transpose=arr;
+// int transposeRows=transpose.size();
+// for(int i=0;i<transposeRows;i++){
+//     for(int j=0;j<transpose[i].size();j++){
+//         if(i<=j){
+//             transpose[i][j]=transpose[j][i];
+//         }
+//     }
+// }
+// cout<<"trasnspose matrix: "<<endl;
+// for(int i=0;i<transposeRows;i++){
+//     for(int j=0;j<transpose[i].size();j++){
+//        cout<<transpose[i][j]<<" ";
+//     }
+//     cout<<endl;
+// }
+// }
+
+
+// int main(){
+//     vector<vector<int>>test(3,vector<int>(3));
+// trasnsposeMatrix(test);
+
+
+
+//     return 0;
+// 
+#include <iostream>
+#include <vector>
+using namespace std;
+void transpose(vector<vector<int>>&arr){
+    int rows=arr.size();
+    int cols=arr[0].size();
+    cout<<"taking inputs: "<<endl;
+    for(int i=0;i<rows;i++){
+        for(int j=0;j<arr[i].size();j++){
+            cin>>arr[i][j];
         }
     }
-}
-cout<<"trasnspose matrix: "<<endl;
-for(int i=0;i<transposeRows;i++){
-    for(int j=0;j<transpose[i].size();j++){
-       cout<<transpose[i][j]<<" ";
+    //print array
+    for(int i=0;i<rows;i++){
+        for(int j=0;j<arr[i].size();j++){
+         cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
     }
-    cout<<endl;
-}
-}
+    
+    //transpose
 
-
+    vector<vector<int>> ulta(cols, vector<int>(rows));
+    for(int i=0;i<rows;i++){
+        for(int j=0;j<cols;j++){
+          ulta[j][i]=arr[i][j];
+        }
+    }
+    //printing
+    cout<<"transpose: "<<endl;
+    
+     for(int i=0;i<cols;i++){
+        for(int j=0;j<rows;j++){
+            cout<<ulta[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
 int main(){
-    vector<vector<int>>test(3,vector<int>(3));
-trasnsposeMatrix(test);
+    vector<vector<int>>test(3,vector<int>(4));
+    transpose(test);
 
-
-
-    return 0;
 }
