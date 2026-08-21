cefce817725698f61d84b1a470535e41d15b2299
rahulkumarprasad07 <rahul.kp1024@gmail.com>
2026-08-21 19:00:11 +0530

    adding

---
+++ ARRAYS/finalVector.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+void boundarySum(vector<vector<int>>&matrix){
+    int row=matrix.size();
+    int col=matrix[0].size();
+    cout<<"enter inputs:"<<endl;
+    for(int i=0;i<row;i++){
+        for(int j=0;j<col;j++){
+            cin>>matrix[i][j];
+        }
+    }
+    cout<<"matrix:"<<endl;
+        for(int i=0;i<row;i++){
+        for(int j=0;j<col;j++){
+            cout<<matrix[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    //boundary sum...
+    int top=0,bottom=row-1;
+    int left=0,right=col-1;
+    int sum=0;
+    //left to right
+    for(int i=top;i<=right;i++){
+    sum+=matrix[top][i];
+    }
+    top++;
+    //top to bottom
+    for(int i=right;i<=bottom;i++){
+   sum+=matrix[i][right];
+    }
+    right--;
+    //right to left
+    for(int i=right;i>=left;i--){
+        sum+=matrix[bottom][i];
+    }
+    bottom--;
+    //bottom to top
+    for(int i=bottom;i>=top;i--){
+        sum+=matrix[i][left];
+    }
+    cout<<"sum is :"<<sum<<endl;
+}
+
+
+
+
+
+int main(){
+ vector<vector<int>>test(3,vector<int>(2));
+ boundarySum(test);
+    return 0;
+}
