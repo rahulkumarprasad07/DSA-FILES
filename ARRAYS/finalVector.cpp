7313137abb4a5e3c002b6f92d90fecfd4ba0b691
rahulkumarprasad07 <rahul.kp1024@gmail.com>
2026-08-21 20:25:01 +0530

    adding

--- ARRAYS/finalVector.cpp
+++ ARRAYS/finalVector.cpp
@@ -52,10 +52,57 @@
 //  boundarySum(test);
 //     return 0;
 // }
+// #include <iostream>
+// #include<vector>
+// using namespace std;
+// void boundarySum(vector<vector<int>>&matrix){
+//     int row=matrix.size();
+//     int col=matrix[0].size();
+//     cout<<"enter inputs:"<<endl;
+//     for(int i=0;i<row;i++){
+//         for(int j=0;j<col;j++){
+//             cin>>matrix[i][j];
+//         }
+//     }
+//     cout<<"matrix:"<<endl;
+//         for(int i=0;i<row;i++){
+//         for(int j=0;j<col;j++){
+//             cout<<matrix[i][j]<<" ";
+//         }
+//         cout<<endl;
+//     }
+//     //transpose
+//     vector<vector<int>> transpose(col, vector<int>(row));
+    
+//     for(int i=0;i<row;i++){
+//         for(int j=0;j<col;j++){
+//             transpose[j][i]=matrix[i][j];
+//         }
+//     }
+//     //transpose matrix
+//     cout<<"transpose matrix:"<<endl;
+//         for(int i=0;i<col;i++){
+//         for(int j=0;j<row;j++){
+//             cout<<transpose[i][j]<<" ";
+//         }
+//         cout<<endl;
+//     }
+
+// }
+
+
+
+
+// int main(){
+//  vector<vector<int>>test(3,vector<int>(2));
+//  boundarySum(test);
+//     return 0;
+// }
+
 #include <iostream>
 #include<vector>
 using namespace std;
-void boundarySum(vector<vector<int>>&matrix){
+void nintyDegree(vector<vector<int>>&matrix){
     int row=matrix.size();
     int col=matrix[0].size();
     cout<<"enter inputs:"<<endl;
@@ -71,22 +118,33 @@ void boundarySum(vector<vector<int>>&matrix){
         }
         cout<<endl;
     }
-    //transpose
-    vector<vector<int>> transpose(col, vector<int>(row));
-    
-    for(int i=0;i<row;i++){
-        for(int j=0;j<col;j++){
-            transpose[j][i]=matrix[i][j];
-        }
+   vector<vector<int>>ulta(col,vector<int>(row));
+   for(int i=0;i<row;i++){
+    for(int j=0;j<col;j++){
+        ulta[j][i]=matrix[i][j];
     }
-    //transpose matrix
-    cout<<"transpose matrix:"<<endl;
-        for(int i=0;i<col;i++){
-        for(int j=0;j<row;j++){
-            cout<<transpose[i][j]<<" ";
-        }
-        cout<<endl;
+   }
+   //printing Traanspose
+   cout<<"transpose"<<endl;
+   for(int i=0;i<col;i++){
+for(int j=0;j<row;j++){
+    cout<<ulta[i][j]<<" ";
+}
+cout<<endl;
+   }
+   //reversing
+for(int i=0; i<col; i++){
+    for(int j=0; j<row/2; j++){
+        swap(ulta[i][j], ulta[i][row-1-j]);
     }
+}
+cout<<"reverse:"<<endl;
+   for(int i=0;i<col;i++){
+for(int j=0;j<row;j++){
+    cout<<ulta[i][j]<<" ";
+}
+cout<<endl;
+   }
 
 }
 
@@ -95,6 +153,7 @@ void boundarySum(vector<vector<int>>&matrix){
 
 int main(){
  vector<vector<int>>test(3,vector<int>(2));
- boundarySum(test);
+ nintyDegree(test);
     return 0;
 }
+
