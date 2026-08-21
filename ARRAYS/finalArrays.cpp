0cf077d1b2ce9bc0975eafec04886d607a00d6cd
rahulkumarprasad07 <rahul.kp1024@gmail.com>
2026-08-21 18:27:35 +0530

    adding

--- ARRAYS/finalArrays.cpp
+++ ARRAYS/finalArrays.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-void sum(vector<vector<int>>matrix){
+vector<int> sum(vector<vector<int>>matrix){
 
     int row=matrix.size();
     //taking input
@@ -20,11 +20,43 @@ void sum(vector<vector<int>>matrix){
         }
         cout<<endl;
     }
-   int top=0;
-   int bottom=matrix.size()-1;
-   int left=0;
-   int right=matrix[0].size()-1;
+ int n=matrix.size();
+ int m=matrix[0].size();
+int left=0, right=m-1;
+int top=0, bottom=n-1;
+vector<int>ans;
+//left to right
+while(left<=right && top<=bottom){
+for(int i=top;i<=right;i++){
+    ans.push_back(matrix[top][i]);
+}
+top++;
+for(int i=top;i<=bottom;i++){
+    ans.push_back(matrix[i][right]);
+}
+right--;
+if(top<=bottom){
+for(int i=right;i>=left;i--){
+    ans.push_back(matrix[bottom][i]);
+}
 
+bottom--;
+}
+if(left<=right){
+for(int i=bottom;i>=top;i--){
+    ans.push_back(matrix[i][left]);
+}
+left++;
+}
+}
+for(int i : ans) {
+    cout << i << " ";
+}
+
+return ans;
+
+
+}
 int main(){
   vector<vector<int>>test(3,vector<int>(2));
   sum(test);
