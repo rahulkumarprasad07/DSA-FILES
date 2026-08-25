4cccd4c8f07e655ef6b56364193f22774a964cc2
rahulkumarprasad07 <rahul.kp1024@gmail.com>
2026-08-25 18:12:33 +0530

    adding

---
+++ ARRAYS/final.cpp
@@ -0,0 +1,38 @@
+// #include<iostream>
+// #include<vector>
+// #include<algorithm>
+// #include<numeric>
+// using namespace std;
+//  bool even(int i){
+//         return i%2==0;
+//     };
+// int main(){
+   
+// vector<int>arr={10,2,3,3,90};
+// int target=10;
+// auto it=find(arr.begin(),arr.end(),target);
+// cout<<*it<<endl;
+// for(int a:arr)
+
+// cout<<a<<" ";
+
+// auto it=find_if(arr.begin(),arr.end(),even);
+// cout<<*it<<endl;
+// reverse(arr.begin(),arr.end());
+// for(int a:arr)
+// cout<<a<<" ";
+// auto it=find_if(arr.begin(),arr.end(),even);
+// cout<<*it<<endl;
+// auto it=unique(arr.begin(),arr.end());
+// arr.erase(it,arr.end());
+// for(int a:arr)
+// cout<<a<<" ";
+
+// partition(arr.begin(),arr.end(),even);
+// for(int a:arr){
+//     cout<<a<<" ";
+// }
+
+
+//     return 0;
+// }
