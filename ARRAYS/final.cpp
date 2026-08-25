64a0c39f11012dcd6445e4f808d35d6706f6304a
rahulkumarprasad07 <rahul.kp1024@gmail.com>
2026-08-25 18:47:44 +0530

    adding

--- ARRAYS/final.cpp
+++ ARRAYS/final.cpp
@@ -38,22 +38,37 @@
 // }
 
 //numeric algorithm
-#include<vector>
-#include<iostream>
-#include<algorithm>
-#include<numeric>
-using namespace std;
-int main(){
-    vector<int>arr(3);
+// #include<vector>
+// #include<iostream>
+// #include<algorithm>
+// #include<numeric>
+// using namespace std;
+// int main(){
+//     vector<int>arr(3);
     // vector<int>brr={0,4,6,3,5};
 //  int product=inner_product(arr.begin(),arr.end(),brr.begin(),0);in
 // vector<int>right(arr.size());
 // partial_sum(arr.begin(),arr.end(),right.begin());
 // for(int a:right)
 // cout<<a<< " " ;
-iota(arr.begin(),arr.end(),3);
-for(int a:arr)
-cout<<a<<" ";
+// iota(arr.begin(),arr.end(),3);
+// for(int a:arr)
+// cout<<a<<" ";
 
-    return 0;
+//     return 0;
+// }
+//searching algorithm
+#include <iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main(){
+    vector<int>arr={33,33,72,29,77,33,35};
+ int target=33;
+    sort(arr.begin(),arr.end());
+
+  auto p=equal_range(arr.begin(),arr.end(),target);
+  cout<<(p.first-arr.begin())<<endl;
+  cout<<(p.second-arr.begin())<<endl;
+return 0;
 }
