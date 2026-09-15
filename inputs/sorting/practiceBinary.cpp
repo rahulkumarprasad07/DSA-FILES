23ff4bd4b8411b587546f612cd455d931b92201a
rahulkumarprasad07 <rahul.kp1024@gmail.com>
2026-09-15 07:51:24 +0530

    adding 15sep

--- inputs/sorting/practiceBinary.cpp
+++ inputs/sorting/practiceBinary.cpp
@@ -1,17 +1,61 @@
-#include<bits/stdc++.h>
+// #include<bits/stdc++.h>
+// using namespace std;
+// int lowerCase(string &s){
+//     int count=0;
+//     getline(cin,s);
+//     for(int i=0;i<s.length();i++){
+//         if(s[i]>='A' && s[i]<='Z'){
+//             s[i]=s[i]-'A'+'a';
+//         }
+//     }
+//     for(int i=0;i<s.length();i++){
+//         if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'){
+//       count++;
+//         }
+//     }
+//     cout<<"count: "<<count<<"\n";
+//     return count;
+// }
+// int main(){
+// string p;
+// lowerCase(p);
+
+
+
+
+
+
+//     return 0;
+// }
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
 
-char arr[10];
-cin>>arr;
-cout<<"input is: "<<arr<<endl;
+void countChar(string s) {
 
+    getline(cin, s);
 
+    int freq[256] = {0};
 
+    for(int i = 0; i < s.length(); i++) {
 
+        if(s[i] != ' ') {
+            freq[s[i]]++;
+        }
+    }
 
+    for(int i = 0; i < 256; i++) {
 
+        if(freq[i] > 0) {
+            cout << char(i) << " = " << freq[i] << endl;
+        }
+    }
+}
 
+int main() {
+
+    string s;
+
+    countChar(s);
 
     return 0;
 }
--- inputs/sorting/tempCodeRunnerFile.cpp
+++ inputs/sorting/tempCodeRunnerFile.cpp
@@ -1,27 +1,10 @@
-    vector<int> test = {3,2,4,43,21,23};
-
-    sort(test.begin(),test.end());
-
-    int n = test.size();
-    int target = 21;
-
-    int start = 0;
-    int end = n - 1;
-
-    while(start <= end){
-        int mid = start + (end-start)/2;
-
-        if(target == test[mid]){
-            cout << "target at " << mid << endl;
-            return 0;
-        }
-        else if(target < test[mid]){
-            end = mid - 1;
-        }
-        else{
-            start = mid + 1;
-        }
+void value(string s){
+    cout<<"enter string:"<<"\n";
+    getline(cin,s);
+    for(int i=0;i<s.length();i++){
+        cout<<s[i]<<" value is: "<<int(s[i])<<"\n";
     }
-
-    return -1;
-   
+}
+int main(){
+string p;
+value(p);
